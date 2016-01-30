4aa89a19094bf4fbd6d0bc9d88a16996413b15a3
Taylor Hocutt <taylor.hocutt@gmail.com>
2016-01-30 01:49:01 -0500

    added metafiles

---
+++ Assets/Scripts/Game Management/Watcher.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Watcher : MonoBehaviour
+{
+    private const int INTERACTIVE_LAYER_ID = 8;
+
+    public float raycastDepth;
+
+    private InteractibleObject currentInteractibleObject = null;
+
+    public InteractibleObject CurrentInteractibleObject
+    {
+        get { return currentInteractibleObject; }
+        set
+        {
+            if(currentInteractibleObject != value)
+            {
+                currentInteractibleObject.StopInteraction();
+                currentInteractibleObject = value;
+                currentInteractibleObject.StartInteraction();
+            }
+        }
+    }
+
+
+    public void Update()
+    {
+        RaycastHit[] hits = Physics.RaycastAll(transform.position, Vector3.forward, Mathf.Infinity);
+
+        //run through all the hits
+        for(int i = 0; i < hits.Length; i++)
+        {
+            //is thehit in  interactive layer
+            if(hits[i].collider.gameObject.layer == INTERACTIVE_LAYER_ID)
+            {
+                InteractibleObject io = hits[i].collider.gameObject.GetComponent<InteractibleObject>();
+                //does the hit actually have an interactible object?
+                if (io != null)
+                {
+                    //we found a new object!
+                    CurrentInteractibleObject = io;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/InteractibleObjects/InteractibleObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractibleObject : MonoBehaviour
+{
+    public float waitTime = 1f;
+    
+
+    public virtual void StartInteraction()
+    {
+        StartCoroutine(InteractionDelay());
+    }
+
+
+    public virtual void StopInteraction()
+    {
+        StopAllCoroutines();
+    }
+
+
+    public virtual IEnumerator InteractionDelay()
+    {
+        yield return new WaitForSeconds(waitTime);
+        Interact();
+    }
+
+
+    protected virtual void Interact()
+    {
+
+    }
+}
