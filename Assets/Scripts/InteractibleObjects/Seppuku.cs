0717e32708a5696d4e98c510386f66b17d5ce423
Taylor Hocutt <taylor.hocutt@gmail.com>
2016-01-31 14:06:53 -0500

    began work on the seppuku interaction

---
+++ Assets/Scripts/InteractibleObjects/Seppuku.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Seppuku : StarableObject
+{
+    public float initialDelay = 30f;
+
+    private bool canInteract = false;
+
+
+    public override bool IsInteracting
+    {
+        get
+        {
+            return base.IsInteracting;
+        }
+
+        set
+        {
+            if(canInteract)
+            {
+                base.IsInteracting = value;
+            }
+        }
+    }
+
+
+    private IEnumerator InitialCountdown()
+    {
+        yield return new WaitForSeconds(initialDelay);
+        canInteract = true;
+    }
+
+
+    private void Awake()
+    {
+        StartCoroutine(InitialCountdown());
+    }
+}
